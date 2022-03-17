c5e8b7c7b107becbf9270d84b61fe402e5547e61
bessilfie-nyame <benjamin.essilfie-nyame@amalitech.com>
2022-03-17 11:36:36 +0100

    utility update

--- utils.py
+++ utils.py
@@ -5,16 +5,19 @@ import shutil
 import csv
 
 root_path = '/srv/beegfs02/scratch/aegis_guardian/data/Datasets_ML_Pipeline/Drive360Challenge/Drive360Images/'
-label_root = '/srv/beegfs02/scratch/aegis_guardian/data/datasets/finetuning/training/bespeed/speed-main/data'
+target_root = '/srv/beegfs02/scratch/aegis_guardian/data/datasets/finetuning/training/bespeed/speed-main/data'
 
 train_path = os.path.join(root_path, 'drive360challenge_train.csv')
 val_path = os.path.join(root_path, 'drive360challenge_validation.csv')
 
-train_out = '/srv/beegfs02/scratch/aegis_guardian/data/datasets/finetuning/training/bespeed/speed-main/data/train/dataset/rgb'
-val_out = '/srv/beegfs02/scratch/aegis_guardian/data/datasets/finetuning/training/bespeed/speed-main/data/validation/dataset/rgb'
+train_out = os.path.join(target_root, 'train/dataset/rgb')
+val_out = os.path.join(target_root, 'validation/dataset/rgb')
 
-train_label = os.path.join(label_root, 'train_target.csv')
-val_label = os.path.join(label_root, 'val_target.csv')
+train_label = os.path.join(target_root, 'train_target.csv')
+val_label = os.path.join(target_root, 'val_target.csv')
+
+train_annotations = os.path.join(target_root, 'train/dataset/annotations.txt')
+val_annotations = os.path.join(target_root, 'validation/dataset/annotations.txt')
 
 MAX_SPEED = 36.893229
 
@@ -86,14 +89,23 @@ def write_avg_speed(inpath, file_name, normalize=True):
         for speed in avg_speed(inpath, normalize=normalize):
             writer.writerow({'avgCanSpeed': speed})
                     
+def create_annotations(infile, file_name, normalize=False):
+    with open(file_name, "w") as file:
+        n = 1
+        for speed in avg_speed(infile, normalize=normalize):
+            file.writelines(f"rgb/video_{n}  1  10  {speed} \n")
+            n += 1
+
 
 if __name__ == '__main__':
     # batch(train_path, train_out)
     # batch(val_path, val_out)
 
-    write_avg_speed(train_path, train_label, normalize=False)
-    write_avg_speed(val_path, val_label, normalize=False)
+    # write_avg_speed(train_path, train_label, normalize=False)
+    # write_avg_speed(val_path, val_label, normalize=False)
 
+    # create_annotations(train_path, train_annotations, normalize=True)
+    create_annotations(val_path, val_annotations, normalize=True)
     
   
        
